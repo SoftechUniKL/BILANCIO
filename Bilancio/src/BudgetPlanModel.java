bc5b68b82020c3eabf696fa80e73ebbe8cd04076
samuel <samuel_asmelash@hotmail.com>
2015-06-01 14:18:23 +0200

    Zwei Methoden für Dateischreiben und Dateilesen hinzugefügt

--- Bilancio/src/BudgetPlanModel.java
+++ Bilancio/src/BudgetPlanModel.java
@@ -1,15 +1,17 @@
 import java.io.FileNotFoundException;
-
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 
 /**
@@ -24,6 +26,11 @@ public class BudgetPlanModel {
     double Kontostand = 0.0;
 	public BudgetPlanModel() {
 		this.ausgaben = new ArrayList<Posten>();
+		readDatafromFile();
+	
+	}
+	
+	void readDatafromFile(){
 		try {
 			// Zeilenweises Einlesen der Daten
 			CSVReader reader = new CSVReader(new FileReader(filename),'#');
@@ -52,7 +59,38 @@ public class BudgetPlanModel {
 					.println("Formatfehler: Die Datei konnte nicht eingelesen werden!");
 			System.exit(1);
 		}
+	}
+	
+	void writeDataIntoFile(){
 		
+		 CSVWriter writer = null;
+			String[] line = new String[4];
+			String str;
+			try {
+				writer = new CSVWriter(new FileWriter("data/budget.csv"), '#', CSVWriter.NO_QUOTE_CHARACTER);
+				
+				int i = 0;
+				
+				for (Posten p : this.ausgaben) {
+					//
+
+					line[0] = new SimpleDateFormat("dd.MM.yyyy").format(p.getDatum());
+					line[1] = p.getBezeichnung() ;
+					line[2] = Double.toString( p.getBetrag());
+					//line[2] = String.format("%.2f", p.getBetrag());
+					line[3] = p.getKategorie().toString();
+					
+					writer.writeNext(line);
+					i++;
+
+				}
+
+				writer.close();
+				
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 	}
 	//Anfangseingabe/betrag für aktuellen Kontostand 
 	
