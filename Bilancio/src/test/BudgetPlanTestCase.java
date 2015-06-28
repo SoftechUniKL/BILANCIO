204ad4e724d2dd0fb521f7e9c402828e1f45b225
samuel <samuel_asmelash@hotmail.com>
2015-06-28 22:28:27 +0200

    Erste Testcase

--- Bilancio/src/test/BudgetPlanTestCase.java
+++ Bilancio/src/test/BudgetPlanTestCase.java
@@ -1,14 +1,22 @@
 package test;
+
 import static org.junit.Assert.*;
+import model.BudgetPlanModel;
 
 import org.junit.Test;
 
 
 public class BudgetPlanTestCase {
 
+	@SuppressWarnings("deprecation")
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		BudgetPlanModel testBudgetPlanModel = new  BudgetPlanModel();
+		
+		String file =  testBudgetPlanModel.filename;
+		
+		assertEquals("data/budget.csv", file);
 	}
 
 }
