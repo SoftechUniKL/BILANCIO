85a8f6de7710c95d1e00c586bfcc8e1a214bb067
Amanuel <aman.ambesa@gmx.de>
2015-07-13 16:03:54 +0200

    Leere und auskommentierte Zeile gelöscht.

--- Bilancio/src/gui/MyTableModel.java
+++ Bilancio/src/gui/MyTableModel.java
@@ -4,41 +4,34 @@ import java.sql.Date;
 
 import javax.swing.table.DefaultTableModel;
 
-public class MyTableModel extends DefaultTableModel{
-	
+public class MyTableModel extends DefaultTableModel {
+
 	Object rowData[][];
 	Object columnNames[];
 
 	public MyTableModel() {
-		// TODO Auto-generated constructor stub
+
 	}
 
-		private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-		public MyTableModel(Object rowData[][], Object columnNames[]) {
-			super(rowData, columnNames);
-		}
+	public MyTableModel(Object rowData[][], Object columnNames[]) {
+		super(rowData, columnNames);
+	}
 
-		@Override
-		public Class getColumnClass(int col) { 
-			
-			if (col == 0)
-				return  Date.class;
-//			if (col == 3) // second column accepts only Integer values
-//				return Double.class;
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Class getColumnClass(int col) {
 
-			else
-				return String.class; // other columns accept String values
-		}
-		
-		
-		    public boolean isCellEditable(int row, int column)      //override isCellEditable
-		    //PRE:  row > 0, column > 0
-		    //POST: FCTVAL == false always
-		    {
-		        return false;
-		    }
-		}
+		if (col == 0)
+			return Date.class;
 
-	
+		else
+			// andere Spalten akzeprieren nur Stringwerte
+			return String.class; 
+	}
 
+	public boolean isCellEditable(int row, int column){
+		// Tabellenzeilen sind nicht editierbar
+		return false;
+	}
+}
